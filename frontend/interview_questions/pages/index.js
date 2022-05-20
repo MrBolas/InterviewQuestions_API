a5146b2a95613fc2eff45f0a741808f58477447a
João Soares <joao.soares@truphone.com>
2022-05-21 00:50:30 +0100

    Updated the frontend

--- frontend/interview_questions/pages/index.js
+++ frontend/interview_questions/pages/index.js
@@ -14,6 +14,10 @@ export default function Home() {
     }
   )
 
+  const openQuestionSource = useCallback(() => {
+    window.open(randomQuestion['Source'], "_blank")
+  },[])
+
   const toggleAnswer = useCallback(() => {
     setShowAnswer(!showAnswer)
   },[showAnswer])
@@ -28,17 +32,16 @@ export default function Home() {
   },[])
 
   return (
-  <div>
-    <p id='question'>
-      {randomQuestion['Question']}
-    </p>
-    {showAnswer &&
-    <p id='answer'>
-      {randomQuestion['Answer']}
-    </p>
-    }
-    <button onClick={getNewQuestion}>Get new question</button>
-    <button onClick={toggleAnswer}>{`${showAnswer ? "Hide" : "Show"} Answer`}</button>
+  <div className="flex justify-center">
+    <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+      <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">{randomQuestion['Question']}</h5>
+      <p className="text-gray-700 text-base mb-4">
+        {showAnswer && <p className="text-grey-700 text-base">{randomQuestion['Answer']}</p>}
+      </p>
+      <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={getNewQuestion}>Get new question</button>
+      <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={toggleAnswer}>{`${showAnswer ? "Hide" : "Show"} Answer`}</button>
+      <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={openQuestionSource}>{`Source`}</button>
+    </div>
   </div>
   )
 }
